8a8137e31135fec822adab190a4fbb396a8da7bb
enPlace <n.place@protonmail.com>
2021-07-10 17:39:32 -0300

    gallery basic

--- src/gallery.js
+++ src/gallery.js
@@ -1,14 +1,30 @@
 import burial from './gallery-images/burial.jpg'
-import delib from 'src/gallery-images/Deliberate_Distortions_mock.jpg'
-import doges from '.src/gallery-images/doges.jpg'
-import french1 from '.src/gallery-images/french-broad-river-brewery.jpg'
-import french2 from '.src/gallery-images/french-broad.jpg'
-import greenman1 from 'src/gallery-images/greenman.jpg'
-import greenman2 from 'src/gallery-images/GreenMan2.jpg'
-import sellick from 'src/gallery-images/sellick.jpg'
-import funk from 'src/gallery-images/Wicked-Weed-Funkatorium-1.jpg
+import delib from './gallery-images/Deliberate_Distortions_mock.jpg'
+import doges from './gallery-images/doges.jpg'
+import french1 from './gallery-images/french-broad-river-brewery.jpg'
+import french2 from './gallery-images/french-broad.jpg'
+import greenman1 from './gallery-images/greenman.jpg'
+import greenman2 from './gallery-images/GreenMan2.jpg'
+import sellick from './gallery-images/sellick.jpg'
+import funk from './gallery-images/Wicked-Weed-Funkatorium-1.jpg'
+
+const images = [burial, delib, doges, french1, french2, greenman1, greenman2, sellick, funk]
+
 function gallerycontent(){
     let gallery = document.createElement("div")
+    gallery.id = "gallery"
     gallery.classList.add("menu-content", "gallery", "active")
+    populateGallery(gallery)
+    document.getElementById("content").appendChild(gallery)
 
 }
+
+function populateGallery(node){
+    images.forEach(image=>{
+        const newimg = document.createElement("img")
+        newimg.src = image
+        node.appendChild(newimg)
+    })
+}
+
+export{gallerycontent}
--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import{content} from "./content"
 import{homecontent} from "./home"
 import { populateBeers } from "./beer-menu"
 import './style.css'
+import { gallerycontent } from "./gallery"
 
 content()
 homecontent()
 populateBeers()
+gallerycontent()
 
