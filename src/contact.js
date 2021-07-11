55296d1fe9411ca622922e8b2292cd19847d6b3f
enPlace <n.place@protonmail.com>
2021-07-11 13:43:05 -0300

    start contact

---
+++ src/contact.js
@@ -0,0 +1,34 @@
+function contact(){
+    
+}
+
+const pubList = [
+    {
+        name: "Burial Beer Co. ",
+        site: "https://burialbeer.com/"
+    },
+    {
+        name: "Wedge Brewing Co.",
+        site: "https://www.wedgebrewing.com/"
+    },
+    {
+        name: "High-Wire Brewing",
+        site: "https://hiwirebrewing.com/south-slope/"
+    },
+    {
+        name: "Green Man Brewery",
+        site: "https://www.greenmanbrewery.com/"
+    },
+    {
+        name: "The Funkatorium",
+        site: "https://www.wickedweedbrewing.com/location/funkatorium/"
+    },
+    {
+        name: "Catawba Brewing Company",
+        site: "https://catawbabrewing.com/"
+    },
+    {
+        name: "Wicked Weed Brewing Pub",
+        site: "https://www.wickedweedbrewing.com/"
+    },
+]
