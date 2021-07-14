1d1e384f702c6a744aa4620e7e4ed01661463c3d
enPlace <n.place@protonmail.com>
2021-07-14 05:50:33 -0300

    moving styles to main src folder

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@
 import { tabs } from "./tabs"
 import { homecontent } from "./home"
 import { populateBeers } from "./beer-menu"
-import './styles/style.css'
-import './styles/reset.css'
+import './style.css'
+import './reset.css'
 import { gallerycontent, setScroll } from "./gallery"
 import { contact } from './contact'
 import { footer } from "./footer"
